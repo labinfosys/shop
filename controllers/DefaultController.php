18199c35268206be3ed78def4fc8343545a3dd89
Sergey Karaichev <karaichev@gmail.com>
2017-11-10 13:11:02 +0300

    Начало маршрутизатора

---
+++ controllers/DefaultController.php
@@ -0,0 +1,9 @@
+<?php
+
+class DefaultController
+{
+    public function actionIndex()
+    {
+        echo 'Главная страница';
+    }
+}
--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -1,13 +1,21 @@
 <?php
 
+require_once '../models/Product.php';
+
 class ProductController
 {
     public function actionView($id)
     {
         $prod = Product::getById($id);
+        var_dump($prod);
         // include('product/view');
         // return $this->render('product/view', [
         //     'product' => $prod
         // ]);
+    }
+
+    public function actionIndex()
+    {
+        echo 'все товары';
     }
 }

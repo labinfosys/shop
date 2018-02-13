99a39e58aeb327d2d3c5fc31cd8573353fc7a5d9
Sergey Karaichev <karaichev@gmail.com>
2018-02-13 10:47:17 +0300

    Форма создания товара

---
+++ backend/views/products/new.php
@@ -0,0 +1,26 @@
+<?php
+//var_dump($product);
+?>
+<form action="" method="post">
+
+<label for="">
+    Название <input type="text" name="Product[name]">
+</label><br>
+<label for="">
+    Категория <input type="text" name="Product[category_id]">
+</label><br>
+<label for="">
+    Код <input type="text" name="Product[code]">
+</label><br>
+<label for="">
+    Цена <input type="text" name="Product[price]">
+</label><br>
+<label for="">
+    Наличие <input type="text" name="Product[availability]">
+</label><br>
+<label for="">
+    Бренд <input type="text" name="Product[brand]">
+</label><br>
+
+<input type="submit" value="Сохранить">
+</form>
